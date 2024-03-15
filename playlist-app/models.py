3db270a2fc19c073dbd30135efabde19ee6711fd
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2024-03-15 19:22:09 -0400

    Add files via upload

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -7,21 +7,43 @@ db = SQLAlchemy()
 
 class Playlist(db.Model):
     """Playlist."""
+    __tablename__ = "playlists"
 
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    name = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True)
+    description = db.Column(db.String(200),
+                    nullable=True)
 
 
 class Song(db.Model):
     """Song."""
+    __tablename__ = "songs"
 
-    # ADD THE NECESSARY CODE HERE
-
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    title = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True)
+    artist = db.Column(db.String(50),
+                    nullable=False)
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
-    # ADD THE NECESSARY CODE HERE
-
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    playlist_id = db.Column(db.Integer,
+                            db.ForeignKey('playlists.id'),
+                   nullable=False)
+    song_id = db.Column(db.Integer,
+                        db.ForeignKey('songs.id'),
+                    nullable=False)
 
 # DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
